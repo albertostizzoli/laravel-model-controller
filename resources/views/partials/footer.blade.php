0b782c04d884d45a1e9bacbe6cee18782121aeb5
albertostizzoli <albertostizzoli@yahoo.it>
2023-12-20 17:27:55 +0100

    stylized footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,27 @@
-<div>Footer</div>
+<footer class="bg-body-tertiary mt-5 pt-5">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <p class="lead">Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div class="col">
+                <h4 class="text-uppercase">Quick links</h4>
+                <ul class="list-unstyled">
+                    <li>Lorem.</li>
+                    <li>Quas.</li>
+                    <li>Volupt</li>
+                    <li>Esse.</li>
+                </ul>
+            </div>
+            <div class="col">
+                <h4 class="text-uppercase">Privacy Policy</h4>
+                <ul class="list-unstyled">
+                    <li>Lorem.</li>
+                    <li>Quas.</li>
+                    <li>Volupt</li>
+                    <li>Esse.</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
