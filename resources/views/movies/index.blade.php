d46d8382be0b06303829e2f58816f5a2852a4cc5
albertostizzoli <albertostizzoli@yahoo.it>
2023-12-20 16:50:20 +0100

    added print card for movies

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -4,7 +4,24 @@
 
 @section('content')
 
-@foreach($movies as $movie)
-<li><a href="{{route('movies.show', $movie->id)}}">{{$movie->title}}</a></li>
-@endforeach
+    <div class="container mt-2">
+        <div class="row gy-4">
+            <h1 class="text-center">Movies</h1>
+            @foreach ($movies as $movie)
+                <div class="col-12 col-md-4 col-lg-3">
+                    <div class="card">
+                        <img class="card-img-top" src="{{ $movie->image }}" alt="{{ $movie->title }}">
+                        <div class="card-title">
+                            <h5>{{ $movie->original_title }}</h5>
+                        </div>
+                        <div class="d-flex justify-content-around align-items-center">
+                            <h6>{{ $movie->language }}</h6>
+                            <h6>{{ $movie->vote }}</h6>
+                        </div>
+                        <a href="{{ route('movies.show', $movie->id) }}" class="btn btn-primary">Informazioni</a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -1,8 +1,26 @@
 @extends('layouts.app')
 
-@section('title', '$books->title')
+@section('title', 'Movie')
 
 @section('content')
 
-<h1>{{$movie->title}}</h1>
+
+
+    <div class="container mt-2">
+        <div class="row gy-4">
+            <div class="col-12 col-md-3 col-lg-4">
+                <div class="card mt-4">
+                    <img class="card-img-top" src="{{ $movie->image }}" alt="{{ $movie->title }}">
+                    <div class="card-title">
+                        <h5>{{ $movie->original_title }}</h5>
+                    </div>
+                    <div class="d-flex justify-content-around align-items-center">
+                        <h6>{{ $movie->language }}</h6>
+                        <h6>{{ $movie->vote }}</h6>
+                    </div>
+                </div>
+                <a href="{{ route('movies.index') }}" class="btn btn-primary">Indietro</a>
+            </div>
+        </div>
+    </div>
 @endsection
